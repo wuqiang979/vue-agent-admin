99ef4ea9b5d14072b62439df40a425bf4ef5d485
wuqiang <happyboy_tu@163.com>
2020-01-06 00:04:53 +0800

    修改顺序

--- src/views/dashboard/admin/components/PanelGroup.vue
+++ src/views/dashboard/admin/components/PanelGroup.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            New Visits
+            分组数量
           </div>
           <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
         </div>
@@ -20,7 +20,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Messages
+            域名转发数量
           </div>
           <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
         </div>
@@ -33,7 +33,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Purchases
+            总访问次数
           </div>
           <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
         </div>
--- src/views/user/AccountInfo.vue
+++ src/views/user/AccountInfo.vue
@@ -1,3 +1,9 @@
+<!--
+ * @Author: 尼大人
+ * @Date: 2019-12-30 22:03:15
+ * @LastEditors  : 尼大人
+ * @LastEditTime : 2020-01-05 23:54:11
+ -->
 <template>
   <div class="app-container">
     <el-card>
@@ -9,23 +15,23 @@
         </el-col>
         <el-col :xs="24" :sm="12" :lg="8">
           <el-form ref="form" :model="formData" :rules="formRules" label-width="100px">
-            <el-form-item label="手机号码">
-              <el-input v-model="formData.phone" disabled />
+            <el-form-item label="名字">
+              <el-input v-model="userData.username" disabled />
             </el-form-item>
             <el-form-item label="邮箱">
               <el-input disabled />
             </el-form-item>
-            <el-form-item label="名字">
-              <el-input disabled />
+            <el-form-item label="手机号码">
+              <el-input v-model="formData.phone" disabled />
             </el-form-item>
-            <el-form-item label="地址">
-              <el-input disabled />
-            </el-form-item>
-            <el-form-item label="称谓">
+            <!-- <el-form-item label="称谓">
               <el-radio-group v-model="sex">
                 <el-radio :label="1">男</el-radio>
                 <el-radio :label="2">女</el-radio>
-              </el-radio-group>
+              </el-radio-group> -->
+            </el-form-item>
+            <el-form-item label="过期时间">
+              <el-input disabled />
             </el-form-item>
             <el-form-item>
               <el-button type="primary" style="width:100%;">提交修改</el-button>
